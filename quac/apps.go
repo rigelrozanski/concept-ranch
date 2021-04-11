0fb493ccb9d6f65eff1c823d3525bec76315765d
rigelrozanski <rigel.rozanski@gmail.com>
2021-04-11 13:48:22 -0400

    ..

--- quac/apps.go
+++ quac/apps.go
@@ -1,6 +1,7 @@
 package quac
 
 import (
+	"errors"
 	"fmt"
 	"path"
 
@@ -17,21 +18,22 @@ func GetForApp(application string) string {
 	return string(content)
 }
 
-func AppendLineForApp(application, appendLine string) {
+func AppendLineForApp(application, appendLine string) error {
 	tags := []string{"external-use", "app=" + application}
 	ideas := GetAllIdeasNonConsuming()
 	subset := ideas.WithTags(tags).WithText()
 	if len(subset) != 1 {
-		return
+		return errors.New("nothing found with those tags")
 	}
 	idea := subset[0]
 	path := path.Join(IdeasDir, idea.Filename)
 	content, err := common.ReadLines(path)
 	if err != nil {
-		return
+		return err
 	}
 	content = append(content, appendLine)
 	_ = common.WriteLines(content, path)
+	return nil
 }
 
 // get a group of images by tag
